3e727a1ea349f437a81b1a22b7a941044438c0e2
kosarik <maksim.putikov0@gmail.com>
2021-07-19 17:45:55 +0300

    feat: fix zero param test in index.js functions

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 
-exports.min = function min (array) { 
+exports.min = function min (array) { if(typeof array ==="undefined" || !array.length) {return 0}
     let result = array.reduce((a, b) => Math.min(a, b));
     return result;
 }
 
-exports.max = function max (array) {
+exports.max = function max (array) {if(typeof array ==="undefined" || !array.length) {return 0}
     let result=array.reduce((a, b) => Math.max(a, b));
     return result;
 }
 
-exports.avg = function avg (array) {
+exports.avg = function avg (array) {if(typeof array ==="undefined" || !array.length) {return 0}
     let result =array.reduce((a, b) => a+b)/array.length;
     return result;
 }
